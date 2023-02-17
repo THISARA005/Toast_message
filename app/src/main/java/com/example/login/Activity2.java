b79438ea6d22cad140515ba1e9d84c0d3ac3a9f8
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-02-17 19:43:05 +0530

    Get a input value form main activity nad show it in another text field in the acitivity 2 after the click button

--- app/src/main/java/com/example/login/Activity2.java
+++ app/src/main/java/com/example/login/Activity2.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
     Button button2;
     TextView txtView;
+    EditText viewAge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +21,21 @@ public class Activity2 extends AppCompatActivity {
 
         button2=findViewById(R.id.btn2);
         txtView=findViewById(R.id.textView2);
+        viewAge=findViewById(R.id.viewAge);
 
         Intent intent=getIntent();
         String name=intent.getStringExtra("USER NAME");
+        int age =intent.getIntExtra("USER AGE",0);
 
         txtView.setText(name);
+        viewAge.setText(String.valueOf(age));
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
+
             }
         });
     }
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -2,31 +2,38 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button1;
+    EditText txtAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button1=findViewById(R.id.btn1);
+        txtAge=findViewById(R.id.txtAge);
         String name="Thisara Dilshan";
+        Editable age= txtAge.getText();
 
         Intent intent=new Intent(this,Activity2.class);
         intent.putExtra("USER NAME",name);
-
-
+        intent.putExtra("USER AGE",age);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(intent);
             }
         });
